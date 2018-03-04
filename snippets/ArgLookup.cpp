135eac00a43e406606d76fc7f9a01f2e64eaa3c3
neurocod <neurocod@gmail.com>
2018-03-04 15:26:07 +0200

    += snippets/ArgLookup.cpp
    ArgLookup.cpp - logs passed arguments so you can debug what is going on

---
+++ snippets/ArgLookup.cpp
@@ -0,0 +1,32 @@
+//ArgLookup.cpp by Kostya Kozachuk as neurocod
+//Logs passed arguments so you can debug what is going on
+#include <QtCore>
+
+int main(int argc, char *argv[]) {
+	QCoreApplication a(argc, argv);
+	QString path = QString("D:/path/to/save/log/args %1.txt")
+		.arg(QDateTime::currentDateTime().toString("yyyy.MM.dd HH-mm-ss-zzz"));
+	struct File: public QFile {
+		File(QString path): QFile(path) {}
+		void write(QString str) {
+			__super::write(str.toLatin1());
+		}
+	};
+	File f(path);
+	if(!f.open(QIODevice::WriteOnly)) {
+		qDebug() << "Can't open file " << path;
+		return 1;
+	}
+	f.write(QString("argc: %1\n").arg(argc));
+	for(int i = 0; i < argc; ++i) {
+		QString str = QString("%1) %2\n")
+			.arg(i)
+			.arg(QString(argv[i]));
+		qDebug() << str;
+		f.write(str);
+	}
+	qDebug() << "ok";
+	QTextStream qtin(stdin);
+	QString line = qtin.readLine();
+	return 0;
+}
